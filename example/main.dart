c68cfe8f973bbc6ef9ff364611e7a1112a2d918f
Anton Kesy <anton@kesy.de>
2022-01-02 16:24:09 +0100

    create example usage

---
+++ example/main.dart
@@ -0,0 +1,50 @@
+import 'package:observer/observer.dart';
+
+class ObserverExample extends Observer {
+  int value = 0;
+
+  ObserverExample(this.value);
+
+  @override
+  void update(Observable observable, Object arg) {
+    //check which observable called update
+    switch (observable.runtimeType) {
+      case ObservableExample:
+        value = arg as int;
+        break;
+      case ResetObservable:
+        value = 0;
+        break;
+    }
+  }
+}
+
+class ObservableExample extends Observable {
+  // wrapper function to prevent casting errors
+  void setObserver(int number) {
+    notifyObservers(number);
+  }
+}
+
+class ResetObservable extends Observable {}
+
+main() {
+  //create observer
+  final observer0 = ObserverExample(0);
+  final observer1 = ObserverExample(1);
+
+  //create observable and both observer
+  final observable = ObservableExample();
+  observable.addObserver(observer0);
+  observable.addObserver(observer1);
+
+  //create another observable but only add one observer
+  final resetObservable = ResetObservable();
+  observable.addObserver(observer0);
+
+  //notifies all observer
+  observable.notifyObservers(42);
+
+  //resets all observer (here only observer 0)
+  resetObservable.notifyObservers(0);
+}
