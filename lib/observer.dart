3a33ba5488eb20e4dd8e07b763caedddbdc56b79
Anton Kesy <anton@kesy.de>
2022-01-02 16:20:33 +0100

    implement simple observer pattern

--- lib/observer.dart
+++ lib/observer.dart
@@ -1,7 +1,27 @@
 library observer;
 
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
+abstract class Observer {
+  void update(Observable observable, Object arg);
+}
+
+class Observable {
+  final List<Observer> _observers = [];
+
+  bool addObserver(Observer observer) {
+    if (_observers.contains(observer)) {
+      return false;
+    }
+    _observers.add(observer);
+    return true;
+  }
+
+  bool removeObserver(Observer observer) {
+    return _observers.remove(observer);
+  }
+
+  void notifyObservers(Object arg) {
+    for (var observer in _observers) {
+      observer.update(this, arg);
+    }
+  }
 }
